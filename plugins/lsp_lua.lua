abbe58e6930b6d2d5008aaaebafcf9d08c1623f8
Adam Harrison <adamdharrison@gmail.com>
2023-01-01 18:01:58 -0500

    Whoops, not sure how that got reverted.

--- plugins/lsp_lua.lua
+++ plugins/lsp_lua.lua
@@ -1,10 +1,12 @@
 -- mod-version:3 -- lite-xl 2.1
 
 local lspconfig = require "plugins.lsp.config"
+local common = require "core.common"
+local config = require "core.config"
 
 local installed_path = USERDIR .. PATHSEP .. "plugins" .. PATHSEP .. "lsp_lua"
 
-lspconfig.sumneko_lua.setup common.merge({
+lspconfig.sumneko_lua.setup(common.merge({
   command = {
     installed_path .. "/bin/lua-language-server" .. (PLATFORM == "Windows" and ".exe" or ""),
     "-E",
@@ -17,4 +19,4 @@ lspconfig.sumneko_lua.setup common.merge({
       }
     }
   }
-}, config.plugins.lsp_lua or {})
+}, config.plugins.lsp_lua or {}))
