6ad8497b1e0a2822a08699ba9dab6f8fd75f0e7e
Guldoman <giulio.lettieri@gmail.com>
2023-08-18 17:20:05 +0200

    Update `lsp_c` and `lsp_lua`
    Also:
    * Fix `lsp_lua` overwriting default `settings`.
    * Make `lsp_c` and `lsp_rust` merge with their configs.

--- plugins/lsp_c.lua
+++ plugins/lsp_c.lua
@@ -1,9 +1,11 @@
 -- mod-version:3 -- lite-xl 2.1
 
 local lspconfig = require "plugins.lsp.config"
+local common = require "core.common"
+local config = require "core.config"
 
 local installed_path = USERDIR .. PATHSEP .. "plugins" .. PATHSEP .. "lsp_c"
 
-lspconfig.clangd.setup({
-  command = { installed_path .. PATHSEP .. "clangd_15.0.6" .. PATHSEP .. "bin" .. PATHSEP .. "clangd" .. (PLATFORM == "Windows" and ".exe" or "") }
-})
+lspconfig.clangd.setup(common.merge({
+  command = { installed_path .. PATHSEP .. "clangd_16.0.2" .. PATHSEP .. "bin" .. PATHSEP .. "clangd" .. (PLATFORM == "Windows" and ".exe" or "") }
+}, config.plugins.lsp_c or {}))
--- plugins/lsp_rust.lua
+++ plugins/lsp_rust.lua
@@ -1,6 +1,8 @@
 -- mod-version:3
 
 local lspconfig = require "plugins.lsp.config"
+local common = require "core.common"
+local config = require "core.config"
 
 local installed_path = USERDIR .. PATHSEP .. "plugins" .. PATHSEP .. "lsp_rust"
 local filename
@@ -12,6 +14,6 @@ else
   filename = "rust-analyzer-x86_64-unknown-linux-gnu"
 end
 
-lspconfig.rust_analyzer.setup({
+lspconfig.rust_analyzer.setup(common.merge({
   command = { installed_path .. PATHSEP .. filename }
-})
+}, config.plugins.lsp_rust or {}))
