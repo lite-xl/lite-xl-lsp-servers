9f5756c25d1b4237bfc1b9ab42e7d2e0b988aac7
ArtemisX64 <101918084+ArtemisX64@users.noreply.github.com>
2023-12-30 06:53:33 +0900

    Update lsp_zig.lua
    Added the correct path to zls(Sorry, This is my first time using lpm)

--- plugins/lsp_zig.lua
+++ plugins/lsp_zig.lua
@@ -4,18 +4,11 @@ local lspconfig = require "plugins.lsp.config"
 local common = require "core.common"
 local config = require "core.config"
 
-local installed_path = USERDIR .. PATHSEP .. "plugins" .. PATHSEP .. "lsp_zig"
-local zlsfile
+local installed_path = USERDIR .. PATHSEP .. "plugins" .. PATHSEP .. "lsp_zig" .. PATHSEP .. "bin"
+local zlsfile = "zls"
+
 if PLATFORM == "Windows" then
-  zlsfile = "zls-x86_64-windows" .. PATHSEP .. "bin" .. PATHSEP .. "zls.exe"
-elseif PLATFORM == "Mac OS X" then
-  if ARCH == "aarch64-darwin" then
-    zlsfile = "zls-aarch64-macos" .. PATHSEP .. "bin" .. PATHSEP .. "zls"
-  else
-    zlsfile = "zls-x86_64-macos" .. PATHSEP .. "bin" .. PATHSEP .. "zls"
-  end
-else
-  zlsfile = "zls-x86_64-linux" .. PATHSEP .. "bin" .. PATHSEP .. "zls"
+  zlsfile = "zls.exe"
 end
 
 lspconfig.zls.setup(common.merge({
