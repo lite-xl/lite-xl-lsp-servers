1eb6ea81dd8d16a7af5fb070fbd37e1683aa21bd
PerilousBooklet <raffaele.orabona@protonmail.com>
2024-02-06 21:40:44 +0100

    Added tex support via texlab.

---
+++ plugins/lsp_tex.lua
@@ -0,0 +1,24 @@
+-- mod-version:3 -- lite-xl 2.1
+
+local lspconfig = require "plugins.lsp.config"
+local common = require "core.common"
+local config = require "core.config"
+
+local installed_path = USERDIR .. PATHSEP .. "plugins" .. PATHSEP .. "lsp_tex"
+
+local texfile
+if PLATFORM == "Windows" then
+  texfile = "texlab.exe"
+elseif PLATFORM == "Mac OS X" then
+  if ARCH == "aarch64-darwin" then
+    texfile = "texlab"
+  else
+    texfile = "texlab"
+  end
+else
+  texfile = "texlab"
+end
+
+lspconfig.texlab.setup(common.merge({
+  command = { installed_path .. PATHSEP .. texfile }
+}, config.plugins.lsp_tex or {}))
