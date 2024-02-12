ffbfe56ad6acd7093edafcfc3ed9fe566edcb89e
Gaspartcho <93390411+Gaspartcho@users.noreply.github.com>
2024-02-12 16:15:38 +0000

    Update lsp_python.lua
    Linked the custom node binary

--- plugins/lsp_python.lua
+++ plugins/lsp_python.lua
@@ -4,8 +4,20 @@ local lspconfig = require "plugins.lsp.config"
 local common = require "core.common"
 local config = require "core.config"
 
-local installed_path = USERDIR .. PATHSEP .. "plugins" .. PATHSEP .. "lsp_python"
+local installed_path_plugin = USERDIR .. PATHSEP .. "plugins" .. PATHSEP .. "lsp_python"
+local installed_path_library = USERDIR .. PATHSEP .. "libraries" .. PATHSEP .. "node"
+
+local server_path = installed_path_plugin .. PATHSEP .. "package" .. PATHSEP .. "langserver.index.js"
+
+local node_path
+if PLATFORM == "Linux" or PLATFORM == "Mac OS X" then
+  node_path = installed_path_library .. PATHSEP .. "bin" .. PATHSEP .. "node"
+elseif PLATFORM == "Windows" then
+  node_path = installed_path_library .. PATHSEP .. "node.exe"
+end
+
+
 
 lspconfig.pyright.setup(common.merge({
-  command = {installed_path .. PATHSEP .. "package" .. PATHSEP .. "langserver.index.js", "--stdio"}
-}, config.plugins.lsp_python or {}))
+  command = {node_path, server_path, "--stdio"}},
+  config.plugins.lsp_python or {}))
