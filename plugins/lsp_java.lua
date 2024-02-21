d477563c267465bd3ea7b336489c99278ee33424
PerilousBooklet <raffaele.orabona@protonmail.com>
2024-02-21 16:10:34 +0100

    Fixed MacOS command.

--- plugins/lsp_java.lua
+++ plugins/lsp_java.lua
@@ -7,26 +7,16 @@ local lspconfig = require "plugins.lsp.config"
 local installed_path_plugin = USERDIR .. PATHSEP .. "plugins" .. PATHSEP .. "lsp_java"
 local installed_path_library = USERDIR .. PATHSEP .. "libraries" .. PATHSEP .. "jdk"
 
-local java_home
+local java_home = installed_path_library .. PATHSEP .. "jdk-21.0.2"
 local java_command
 
 if PLATFORM == "Windows" then
-  java_home = installed_path_library .. PATHSEP .. "jdk-21.0.2" .. PATHSEP .. "bin" .. PATHSEP .. "java.exe"
   if system.get_file_info(java_home) == nil then
     java_command = installed_path_plugin .. PATHSEP .. "bin" .. PATHSEP .. "jdtls"
   else
-    java_command = "JAVA_HOME=" .. java_home .. " " .. installed_path_plugin .. PATHSEP .. "bin" .. PATHSEP .. "jdtls"
-  end
-elseif PLATFORM == "Mac OS X" then
-  java_home = installed_path_library .. PATHSEP .. "jdk-21.0.2.jdk" .. PATHSEP .. "Contents" .. PATHSEP .. "Home" .. PATHSEP .. "bin" .. PATHSEP .. "java"
-  if system.get_file_info(java_home) == nil then
-    java_command = installed_path_plugin .. PATHSEP .. "bin" .. PATHSEP .. "jdtls"
-  else
-    java_command = "JAVA_HOME=" .. java_home .. " " .. installed_path_plugin .. PATHSEP .. "bin" .. PATHSEP .. "jdtls"
+    java_command = ""
   end
 else
-  -- GNU/Linux
-  java_home = installed_path_library .. PATHSEP .. "jdk-21.0.2"
   if system.get_file_info(java_home) == nil then
     java_command = { installed_path_plugin .. PATHSEP .. "bin" .. PATHSEP .. "jdtls" }
   else
