2a0640852bcf7504669df398576e3203eabc0c16
Gaspartcho <93390411+Gaspartcho@users.noreply.github.com>
2024-02-22 13:39:48 +0000

    Update lsp_python.lua
    updated nodejs library install path + logs which node it uses (native or local)

--- plugins/lsp_python.lua
+++ plugins/lsp_python.lua
@@ -3,26 +3,35 @@
 local lspconfig = require "plugins.lsp.config"
 local common = require "core.common"
 local config = require "core.config"
+local core = require "core"
 
 local installed_path_plugin = USERDIR .. PATHSEP .. "plugins" .. PATHSEP .. "lsp_python"
+local node_version = "node-v20.11.0"
 local installed_path_library = USERDIR .. PATHSEP .. "libraries" .. PATHSEP .. "nodejs"
 
 local server_path = installed_path_plugin .. PATHSEP .. "package" .. PATHSEP .. "langserver.index.js"
 
 local node_path
-if PLATFORM == "Linux" or PLATFORM == "Mac OS X" then
-  node_path = installed_path_library .. PATHSEP .. "bin" .. PATHSEP .. "node"
-elseif PLATFORM == "Windows" then
-  node_path = installed_path_library .. PATHSEP .. "node.exe"
+if ARCH == "x86_64-linux" then
+  node_path = installed_path_library .. PATHSEP .. node_version .. "-linux-x64" .. PATHSEP .. "bin" .. PATHSEP .. "node"
+elseif ARCH == "x86_64-darwin" then
+  node_path = installed_path_library .. PATHSEP .. node_version .. "-darwin-x64" .. PATHSEP .. "bin" .. PATHSEP .. "node"
+elseif ARCH == "arm64-darwin" then
+  node_path = installed_path_library .. PATHSEP .. node_version .. "-darwin-arm64" .. PATHSEP .. "bin" .. PATHSEP .. "node"
+elseif ARCH == "x86_64-windows" then
+  node_path = installed_path_library .. PATHSEP .. node_version .. "-win-x64" .. PATHSEP .. "node.exe"
 end
 
 local final_command
+core.log(node_path)
 if system.get_file_info(node_path) == nil then
   -- NodeJs not installed with lpm, then tries to use the system path variable.
   final_command = {"node", server_path, "--stdio"}
+  core.log("[lsp_python]: nodejs library not found, trying to use the 'node' path variable.")
 else
   -- NodeJs installed with lpm, runs it instead.
   final_command = {node_path, server_path, "--stdio"}
+  core.log("[lsp_python]: nodejs library found, using it to launch Pyright.")
 end
 
 
