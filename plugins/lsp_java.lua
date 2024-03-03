10c9385d45fe7422e02b2dbd7e98d5b9658de164
PerilousBooklet <raffaele.orabona@protonmail.com>
2024-03-04 00:49:57 +0100

    WIP: jdtls manual command

--- plugins/lsp_java.lua
+++ plugins/lsp_java.lua
@@ -7,20 +7,41 @@ local lsp = require "plugins.lsp"
 local installed_path_plugin = USERDIR .. PATHSEP .. "plugins" .. PATHSEP .. "lsp_java"
 local installed_path_library = USERDIR .. PATHSEP .. "libraries" .. PATHSEP .. "jdk"
 
-local java_home = installed_path_library .. PATHSEP .. "jdk-21.0.2"
-local java_command
-
+local platform
 if PLATFORM == "Windows" then
-  java_command = { installed_path_plugin .. PATHSEP .. "bin" .. PATHSEP .. "jdtls" }
+  platform = "win"
+elseif PLATFORM == "Mac OS X" then
+  platform = "mac"
 else
-  java_command = { installed_path_plugin .. PATHSEP .. "bin" .. PATHSEP .. "jdtls" }
+  platform = "linux"
 end
+
+local jdtls_data_path = system.absolute_path(".") .. PATHSEP .. ".jdtls"
+system.mkdir(jdtls_data_path)
+local version_name  = "1.6.600.v20231106-1826"
+local jdtls_command = { "java", " ",
+                        "-Declipse.application=org.eclipse.jdt.ls.core.id1", " ",
+                        "-Dosgi.bundles.defaultStartLevel=4", " ",
+                        "-Declipse.product=org.eclipse.jdt.ls.core.product", " ",
+                        "-Dlog.level=ALL", " ",
+                        "-Xmx1G", " ",
+                        "--add-modules=ALL-SYSTEM", " ",
+                        "--add-opens", "java.base/java.util=ALL-UNNAMED", " ",
+                        "--add-opens", "java.base/java.lang=ALL-UNNAMED", " ",
+                        "-jar", " ", string.format("./plugins/org.eclipse.equinox.launcher_%s.jar", version_name), " ",
+                        "-configuration", " ", string.format("./config_%s", platform), " ",
+                        "-data", " ", string.format("%s", jdtls_data_path) }
+
+local java_home = installed_path_library .. PATHSEP .. "jdk-21.0.2"
+
+local core = require "core"
+core.log_quiet("JDTLS command: " .. common.serialize(table.concat(jdtls_command)))
 
 lsp.add_server(common.merge({
   name = "jdtls",
   language = "java",
   file_patterns = { "%.java$" },
-  command = java_command,
+  command = jdtls_command,
   env = { ["JAVA_HOME"] = java_home },
   verbose = false
 }, config.plugins.lsp_java or {}))
