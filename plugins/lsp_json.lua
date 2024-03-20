96b732837bfc3d998b9745bd02a0abb8db577bc1
Guldoman <giulio.lettieri@gmail.com>
2024-03-20 04:28:27 +0100

    Add `lsp_json` via `vscode-json-languageserver` with patches

---
+++ plugins/lsp_json.lua
@@ -0,0 +1,12 @@
+-- mod-version:3
+
+local lspconfig = require "plugins.lsp.config"
+local common = require "core.common"
+local config = require "core.config"
+
+local installed_path = USERDIR .. PATHSEP .. "plugins" .. PATHSEP .. "lsp_json" .. PATHSEP .. "vscode-json-languageserver" .. PATHSEP .. "dist" .. PATHSEP .. "index.js"
+local node = require "libraries.nodejs"
+
+lspconfig.jsonls.setup(common.merge({
+  command = { node.path_bin, installed_path, "--stdio" },
+}, config.plugins.lsp_json or {}))
