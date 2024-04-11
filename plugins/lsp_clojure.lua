64ac4c573d397b9440d577e8784e1eff5d8a3dc6
Rohan Vashisht <81112205+RohanVashisht1234@users.noreply.github.com>
2024-04-11 21:10:29 +0530

    Update lsp_clojure.lua

--- plugins/lsp_clojure.lua
+++ plugins/lsp_clojure.lua
@@ -7,23 +7,7 @@ local common = require "core.common"
 local config = require "core.config"
 
 local installed_path = USERDIR .. PATHSEP .. "plugins" .. PATHSEP .. "lsp_clojure"
-local filename
-if PLATFORM == "Windows" then
-  filename = "clojure-lsp.exe"
-elseif PLATFORM == "Mac OS X" then
-  if ARCH == "aarch64-darwin" then
-    filename = "clojure-lsp"
-  else
-    filename = "clojure-lsp"
-  end
-else
-  if ARCH == "aarch64-linux" then
-    filename = "clojure-lsp"
-  else
-    filename = "clojure-lsp"
-  end
-end
 
-lspconfig.rust_analyzer.setup(common.merge({
+lspconfig.clojure_lsp.setup(common.merge({
   command = { installed_path .. PATHSEP .. filename }
-}, config.plugins.lsp_rust or {}))
+}, config.plugins.lsp_clojure or {}))
