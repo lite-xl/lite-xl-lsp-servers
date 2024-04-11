083a31463218654fbf92cf12e4f3422606b36e4d
Rohan Vashisht <81112205+RohanVashisht1234@users.noreply.github.com>
2024-04-11 23:54:57 +0530

    Update lsp_clojure.lua

--- plugins/lsp_clojure.lua
+++ plugins/lsp_clojure.lua
@@ -9,5 +9,5 @@ local config = require "core.config"
 local installed_path = USERDIR .. PATHSEP .. "plugins" .. PATHSEP .. "lsp_clojure"
 
 lspconfig.clojure_lsp.setup(common.merge({
-  command = { installed_path .. PATHSEP .. "clojure-lsp" }
+  command = { installed_path .. PATHSEP .. "clojure-lsp" .. (PLATFORM == "Windows" and ".exe" or "") }
 }, config.plugins.lsp_clojure or {}))
