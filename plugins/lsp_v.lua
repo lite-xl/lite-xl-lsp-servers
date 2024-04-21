0751113d5ad67dc04981575282107953a876fa8d
Rohan Vashisht <81112205+RohanVashisht1234@users.noreply.github.com>
2024-04-22 00:18:12 +0530

    Update lsp_v.lua

--- plugins/lsp_v.lua
+++ plugins/lsp_v.lua
@@ -1,1 +1,13 @@
+-- Author: Rohan Vashisht: https://github.com/RohanVashisht1234
 
+-- mod-version:3
+
+local lspconfig = require "plugins.lsp.config"
+local common = require "core.common"
+local config = require "core.config"
+
+local installed_path = USERDIR .. PATHSEP .. "plugins" .. PATHSEP .. "lsp_v"
+
+lspconfig.v_lsp.setup(common.merge({
+  command = { installed_path .. PATHSEP .. "v-analyzer" .. (PLATFORM == "Windows" and ".exe" or "") }
+}, config.plugins.lsp_v or {}))
