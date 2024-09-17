88652e507ef7578026e08cc202557fd88f313096
PerilousBooklet <raffaele.orabona@protonmail.com>
2024-09-17 17:49:02 +0200

    Removed lsp_tex reference.

--- plugins/lsp_d.lua
+++ plugins/lsp_d.lua
@@ -8,4 +8,4 @@ local installed_path = USERDIR .. PATHSEP .. "plugins" .. PATHSEP .. "lsp_d"
 
 lspconfig.serve_d.setup(common.merge({
   command = { installed_path .. PATHSEP .. "serve-d" .. (PLATFORM == "Windows" and ".exe" or "")}
-}, config.plugins.lsp_tex or {}))
+}, config.plugins.lsp_d or {}))
