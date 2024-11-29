acba0e9c89b449f9aff9cba70781a0a9eca9b0f4
SnakE-2024 <snake6snake@gmail.com>
2024-11-29 12:50:34 +0200

    major changes & improvements

--- README.md
+++ README.md
@@ -42,3 +42,4 @@ These libraries are dependencies of some language servers (automatic installatio
 | :------------------------------------ | :------------------------------------------ | :------------------ |
 | [jdk](/libraries/jdk.lua?raw=1)       | JDK binaries for running java apps          | Linux, Mac, Windows |
 | [nodejs](/libraries/nodejs.lua?raw=1) | NodeJs binaries for running javascript apps | Linux, Mac, Windows |
+| [haxe](/libraries/haxe.lua?raw=1)     | Haxe binaries for running haxe apps         | Linux, Mac, Windows |
--- libraries/haxe.lua
+++ libraries/haxe.lua
@@ -1,8 +1,11 @@
 local info = {}
 
 info.version = "haxe-4.3.6"
-info.path_lib = USERDIR .. PATHSEP .. "libraries" .. PATHSEP .. "haxe"
 info.haxe_dir = "haxe_20240807093059_760c0dd"
-info.path_bin = info.path_lib .. PATHSEP .. info.haxe_dir .. PATHSEP .. "haxe" .. (PLATFORM == "Windows" and ".exe" or "")
+info.path_lib = USERDIR .. PATHSEP .. "libraries" .. PATHSEP .. "haxe"
+info.path_bin_haxe = info.path_lib .. PATHSEP .. info.haxe_dir .. PATHSEP .. "haxe" .. (PLATFORM == "Windows" and ".exe" or "")
+info.path_bin_haxelib = info.path_lib .. PATHSEP .. info.haxe_dir .. PATHSEP .. "haxelib" .. (PLATFORM == "Windows" and ".exe" or "")
+
+info.path_bin = info.path_bin_haxe -- Same as `path_bin_haxe`
 
 return info
