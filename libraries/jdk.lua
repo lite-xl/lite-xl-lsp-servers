a1d66e32f0b4ac1066fcf7376d3c018871988b90
Guldoman <giulio.lettieri@gmail.com>
2025-03-13 20:37:05 +0100

    chore: update `jdk` (#59)

--- libraries/jdk.lua
+++ libraries/jdk.lua
@@ -1,6 +1,6 @@
 local info = {}
 
-info.version = "jdk-22.0.1"
+info.version = "jdk-23.0.2"
 info.path_lib = USERDIR .. PATHSEP .. "libraries" .. PATHSEP .. "jdk"
 info.path_bin = info.path_lib .. PATHSEP .. info.version .. PATHSEP .. "bin" .. PATHSEP .. "java" .. (PLATFORM == "Windows" and ".exe" or "")
 
