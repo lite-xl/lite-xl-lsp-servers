5278737e8fc4d0a1818c3a0bdd3351cd37d3e03d
Guldoman <giulio.lettieri@gmail.com>
2025-04-29 00:15:32 +0200

    feat: add `golang` library and `lsp_go` plugin

---
+++ libraries/golang.lua
@@ -0,0 +1,9 @@
+local info = {}
+
+info.version = "go-1.24.2"
+info.path_lib = USERDIR .. PATHSEP .. "libraries" .. PATHSEP .. "golang" .. PATHSEP .. "go"
+info.path_bindir = info.path_lib .. PATHSEP .. "bin"
+info.path_bin = info.path_bindir .. PATHSEP .. "go" .. (PLATFORM == "Windows" and ".exe" or "")
+info.path_bin_fmt = info.path_bindir .. PATHSEP .. "gofmt" .. (PLATFORM == "Windows" and ".exe" or "")
+
+return info
