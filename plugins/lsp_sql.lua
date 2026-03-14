9e57887f60fef35fe8da205c64065c6f5fb00912
PerilousBooklet <raffaele.orabona@protonmail.com>
2026-03-14 17:11:47 +0100

    refactor: renamed bin file path variable to shorter name

--- plugins/lsp_sql.lua
+++ plugins/lsp_sql.lua
@@ -4,12 +4,12 @@ local common = require "core.common"
 local config = require "core.config"
 local lsp = require "plugins.lsp"
 
-local installed_path_plugin = USERDIR .. PATHSEP .. "plugins" .. PATHSEP .. "lsp_sql"
+local installed_path = USERDIR .. PATHSEP .. "plugins" .. PATHSEP .. "lsp_sql"
 
 lsp.add_server(common.merge({
   name = "sqls",
   language = "sql",
   file_patterns = { "%.sql$" },
-  command = { installed_path_plugin .. PATHSEP .. 'sqls' },
+  command = { installed_path .. PATHSEP .. 'sqls' },
   verbose = false
 }, config.plugins.lsp_sql or {}))
