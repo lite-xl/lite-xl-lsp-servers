3d77d7c38f3ae0620f00907aa3b75baba85e1660
Raffaele Orabona <raffaele.orabona@protonmail.com>
2026-07-17 13:10:39 +0200

    Add SQL LSP bundle with `sqls` (#71)
    * feat: added SQL LSP bundle with sqls
    
    * refactor: renamed bin file path variable to shorter name
    
    * feat: added support for Windows
    
    * chore: updated to version 0.2.48

---
+++ plugins/lsp_sql.lua
@@ -0,0 +1,15 @@
+--mod-version:3
+local core = require "core"
+local common = require "core.common"
+local config = require "core.config"
+local lsp = require "plugins.lsp"
+
+local installed_path = USERDIR .. PATHSEP .. "plugins" .. PATHSEP .. "lsp_sql"
+
+lsp.add_server(common.merge({
+  name = "sqls",
+  language = "sql",
+  file_patterns = { "%.sql$" },
+  command = { installed_path .. PATHSEP .. 'sqls' .. (PLATFORM == "Windows" and ".exe" or "") },
+  verbose = false
+}, config.plugins.lsp_sql or {}))
